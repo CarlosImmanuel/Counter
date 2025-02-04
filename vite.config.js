287ce53419bcc7ea12fc7ad7f12b910da6b5bf58
CarlosImmanuel <carlosimmanuel1208@gmail.com>
2025-02-04 20:01:47 +0700

    Penghitung Angkaa (Counter App)

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import ghPages from 'vite-plugin-gh-pages'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/https://github.com/CarlosImmanuel/Counter.git/'
 })
