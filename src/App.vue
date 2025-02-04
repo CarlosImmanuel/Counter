7514c15ef1958233e7078f7d3bee18568d436691
CarlosImmanuel <carlosimmanuel1208@gmail.com>
2025-02-04 19:39:58 +0700

    Penghitung Angka (Counter App)

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Counter from './components/Counter.vue'
+</script>
+
+<template>
+  <main>
+    <Counter />
+  </main>
+</template>
+
+<style>
+html, body {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+}
+
+main {
+  justify-content: center;
+  align-items: center;
+  background-color: black;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+}
+</style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { ref } from "vue";
+
+const count = ref(0);
+
+const increment = () => count.value++;
+const decrement = () => count.value--;
+const reset = () => (count.value = 0);
+</script>
+
+<template>
+    <div class="counter">
+        <h1 style="color: white;">Counter App</h1>
+        <p class="count"> {{ count }}</p>
+        <div class="buttons">
+            <button @click="decrement">
+                -
+            </button>
+            <button @click="increment">
+                +
+            </button>
+            <button @click="reset">
+                Reset
+            </button>
+        </div>
+    </div>
+</template>
+
+<style>
+.counter {
+    text-align: center;
+    margin-top: 10px;
+}
+
+.count {
+    font-size: 2rem;
+    margin: 10px;
+    color: white;
+}
+
+.buttons button {
+    margin: 5px;
+    padding: 10px 15px;
+    font-size: 1.2rem;
+    cursor: pointer;
+}
+</style>
